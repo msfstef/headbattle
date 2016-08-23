54233b4bb60576da976bf57931c5ad0e077e5073
Stefanos Mousafeiris <msfstef@gmail.com>
2016-08-23 23:28:34 +0300

    Improved collisions
    Issues with horizontal collisions, must fix. Changed to momentum collisions.

--- headbattle.js
+++ headbattle.js
@@ -4,6 +4,31 @@ var canvas = document.getElementById('canvasHB');
 var ctx = canvas.getContext('2d');
 
 var p1_ctrls = new Array(87,83,65,68);
+var spf = 1/60; // Seconds per frame.
+
+
+Object.prototype.clone = Array.prototype.clone = function()
+{
+    if (Object.prototype.toString.call(this) === '[object Array]')
+    {
+        var clone = [];
+        for (var i=0; i<this.length; i++)
+            clone[i] = this[i].clone();
+
+        return clone;
+    } 
+    else if (typeof(this)=="object")
+    {
+        var clone = {};
+        for (var prop in this)
+            if (this.hasOwnProperty(prop))
+                clone[prop] = this[prop].clone();
+
+        return clone;
+    }
+    else
+        return this;
+}
 
 
 
@@ -29,6 +54,7 @@ var Player = function(x,y,ctrls){
 	this.pos = new Vector2d(x,y); // position of head centre
 	this.vel = new Vector2d(0,0);
 	this.acc = new Vector2d(0,0);
+	this.mass = 60;
 	this.headSize = 40;
 	this.init(x,y,ctrls);
 };
@@ -44,7 +70,7 @@ Player.prototype.init = function(x,y,ctrls) {
 	this.vel.set(0,0);
 	this.frozen = false;
 	this.canJump = true;
-	this.jumpSpd = -20;
+	this.jumpSpd = -150*spf;
 };
 
 Player.prototype.draw = function() {
@@ -72,7 +98,7 @@ Player.prototype.draw = function() {
 
 Player.prototype.update = function() {
 	if (this.pos.y < canvas.height - this.headSize*3.7){
-		this.acc.y = 1.5;
+		this.acc.y = 1.5*spf;
 		this.canJump = false;
 	} else {
 		this.pos.y = canvas.height - this.headSize*3.7;
@@ -83,18 +109,18 @@ Player.prototype.update = function() {
 
 	if (this.rightPress && !this.leftPress) {
 		if (Math.sign(this.vel.x) == -1){
-			this.acc.x = 2.5;
+			this.acc.x = 2.5*spf;
 		} else {
-			this.acc.x = 1;
+			this.acc.x = 1*spf;
 		}
 	} else if (this.leftPress && !this.rightPress) {
 		if (Math.sign(this.vel.x) == 1){
-			this.acc.x = -2.5;
+			this.acc.x = -2.5*spf;
 		} else {
-			this.acc.x = -1;
+			this.acc.x = -1*spf;
 		}
-	} else if (Math.abs(this.vel.x) > 1) {
-		this.acc.x = -Math.sign(this.vel.x)*2;
+	} else if (Math.abs(this.vel.x) > 1*spf) {
+		this.acc.x = -Math.sign(this.vel.x)*2*spf;
 	} else {
 		this.acc.x = 0;
 		this.vel.x = 0;
@@ -114,11 +140,11 @@ Player.prototype.update = function() {
 
 	if (this.pos.x - this.headSize < 0) {
 		this.pos.x = 0 + this.headSize;
-		this.vel.x = -0.5*this.vel.x;
+		this.vel.x = -0.5*this.vel.x*spf;
 		this.acc.x = 0;
 	} else if (this.pos.x + this.headSize > canvas.width) {
 		this.pos.x = canvas.width - this.headSize;
-		this.vel.x = -0.5*this.vel.x;
+		this.vel.x = -0.5*this.vel.x*spf;
 		this.acc.x = 0;
 	}
 
@@ -131,7 +157,9 @@ Player.prototype.update = function() {
 var Ball = function(x,y){
 	this.pos = new Vector2d(x,y); // position of head centre
 	this.vel = new Vector2d(0,0);
-	this.acc = new Vector2d(0,0);
+	this.acc = new Vector2d(0,1.5*spf);
+	this.last_pos = new Vector2d(0,0);
+	this.mass = 1;
 	this.size = 30; 
 	this.init(x,y);
 };
@@ -154,14 +182,20 @@ Ball.prototype.draw = function() {
 };
 
 Ball.prototype.update = function(players) {
-	if (this.pos.y < canvas.height - this.size*1.1){
-		this.acc.y = 1.5;
-	} else if (this.pos.y) {
+	if (this.pos.y < this.size) {
+		this.pos.y = this.size;
+		this.vel.y = -this.elast*this.vel.y;
+	} else if (this.pos.y >= canvas.height - this.size) {
 		this.pos.y = canvas.height - this.size;
-		this.acc.y = 1.5;
 		this.vel.y = -this.elast*this.vel.y;
 	}
 
+	if (Math.abs(this.vel.x) > 1*spf &&
+		this.pos.y >= canvas.height - this.size) {
+		this.acc.x = -Math.sign(this.vel.x)*2*spf;
+	} else {
+		this.acc.x = 0;
+	}
 
 	if (this.pos.x - this.size < 0) {
 		this.pos.x = 0 + this.size;
@@ -190,7 +224,7 @@ Ball.prototype.update = function(players) {
 
 	for (var i=0; i < players.length; i++) {
 		var p = players[i];
-		
+		var sizes = this.size + p.headSize;
 		if (Math.sqrt((this.pos.x - p.pos.x)*(this.pos.x - p.pos.x)
 				+ (this.pos.y-p.pos.y)*(this.pos.y-p.pos.y)) < 
 				(this.size + p.headSize)) {
@@ -198,8 +232,8 @@ Ball.prototype.update = function(players) {
 			var posB = [this.pos.x,this.pos.y];
 			var velP = [p.vel.x,p.vel.y];
 			var posP = [p.pos.x,p.pos.y];
-			var mB = 1;
-			var mP = 60;
+			var mB = this.mass;
+			var mP = p.mass;
 
 			this.vel.x =  this.vel.x - ((2*mP/(mB+mP))*(
 				dot(sub(velB,velP),sub(posB,posP)))/dot(sub(posB,posP),sub(posB,posP)))*
@@ -214,29 +248,25 @@ Ball.prototype.update = function(players) {
 			p.vel.y =  p.vel.y - ((2*mB/(mB+mP))*(
 				dot(sub(velP,velB),sub(posP,posB)))/dot(sub(posP,posB),sub(posP,posB)))*
 				sub(posP,posB)[1];
-			console.log(this.vel.x)
-			console.log(p.vel.x)
+
+			this.pos.x = this.last_pos.x.clone();
+			this.pos.y = this.last_pos.y.clone();
 
 
-		} else if (this.pos.y > p.pos.y - p.headSize &&
-			this.pos.y + this.size <= p.pos.y + p.headSize*3.7) {
-			if (this.pos.x - this.size <= p.pos.x + p.headSize*0.4 &&
-				this.pos.x - this.size >= p.pos.x - p.headSize*0.4) {
-
-				this.pos.x = this.size + p.pos.x + p.headSize*0.5;
-				this.vel.x = -this.vel.x + p.vel.x + 1;
-				this.vel.y -= 15;
-
-			} else if (this.pos.x + this.size <= p.pos.x + p.headSize*0.4 &&
-						this.pos.x + this.size >= p.pos.x - p.headSize*0.4){
-
-				this.pos.x = - this.size + p.pos.x - p.headSize*0.5;
-				this.vel.x = -this.vel.x + p.vel.x - 1;
-				this.vel.y -= 15;
+		} else if (this.pos.y - this.size > p.pos.y + p.headSize &&
+			this.pos.y - this.size <= p.pos.y + p.headSize*3.7) {
+			if (Math.abs(this.pos.x - p.pos.x) < this.size + p.headSize*0.5) {
+				this.pos.x = this.last_pos.x.clone();
+				this.pos.y = this.last_pos.y.clone();
+				var vel_init = this.vel.x.clone();
+				this.vel.x = (p.mass/(this.mass+p.mass)*
+							(p.vel.x - this.vel.x))
+				p.vel.x = (this.mass/(this.mass+p.mass)*
+							(p.vel.x - vel_init))
 			}
 		}
 	}
-
+	this.last_pos = this.pos.clone();
 	this.pos.add(this.vel);
 	this.vel.add(this.acc);
 };
@@ -285,8 +315,8 @@ var update = function(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	ball.draw();
 	p1.draw();
-	ball.update(players);
 	p1.update();
+	ball.update(players);
 }
 
-setInterval(update,30);
+setInterval(update,spf);
