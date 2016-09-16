fbf3b2b811f694cebcba90b0cd4a699242d38e23
Stefanos Mousafeiris <msfstef@gmail.com>
2016-09-16 20:28:48 +0300

    Finalised multiplayer.
    Game runs on server, client draws server output and sends raw input for player movement. Physics are run at 100 fps and graphics are refreshed at 60 fps. Users can also choose a name, spectator mode is also included. Still have issues with collisions.

--- client.js
+++ client.js
@@ -1,3 +1,194 @@
+var canvas = document.getElementById('canvasHB');
+var ctx = canvas.getContext('2d');
+
+// Controls are up, down, left, right.
+var p1_ctrls = new Array(87,83,65,68);
+var p2_ctrls = new Array(38,40,37,39);
+var observer;
+var p_name = '';
+
+var keyDown = function(e,p) {
+	if(e.keyCode == p.ctrls[0]) {
+        p.upPress = true;
+    }
+    else if(e.keyCode == p.ctrls[1]) {
+        if (p.kick_timeout) {
+        	p.downPress = true;
+        	p.kick_timeout = false;
+        	setTimeout(function(){
+        		p.kick_timeout=true;
+        	}, 100);
+       	}
+    } 
+    else if(e.keyCode == p.ctrls[2]) {
+        p.leftPress = true;
+    }
+    else if(e.keyCode == p.ctrls[3]) {
+        p.rightPress = true;
+    }
+}
+
+var keyUp = function(e,p) {
+	if(e.keyCode == p.ctrls[0]) {
+        p.upPress = false;
+    }
+    else if(e.keyCode == p.ctrls[1]) {
+        p.downPress = false;
+    } 
+    else if(e.keyCode == p.ctrls[2]) {
+        p.leftPress = false;
+    }
+    else if(e.keyCode == p.ctrls[3]) {
+        p.rightPress = false;
+    }
+}
+
+
+var p_no;
+var p1 = {
+	pos: {x:0,y:0},
+	headSize : 40,
+	goalPost: {pos:{x:0,y:0},
+				w:40*4,
+				h:40*9},
+	upPress: false,
+	downPress: false,
+	leftPress: false,
+	rightPress: false,
+	ctrls: [],
+	colour: 'red',
+	kick_timeout : true
+};
+var p2 = {
+	pos: {x:0,y:0},
+	headSize : 40,
+	goalPost: {pos:{x:0,y:0},
+				w:40*4,
+				h:40*9},
+	upPress: false,
+	downPress: false,
+	leftPress: false,
+	rightPress: false,
+	ctrls: [],
+	colour: 'blue',
+	kick_timeout: true
+};
+var players=[p1,p2];
+var ball = {
+	pos: {x:0,y:0},
+	size: 20
+};
+
+
+var text;
+var score_p1;
+var score_p2;
+var scored;
+
+
+var drawPlayer = function (item) {
+	ctx.beginPath();
+	ctx.font = "45px Arial";
+	ctx.fillStyle = "black";
+	ctx.textAlign = "center";
+	ctx.fillText(item.p_name,
+				item.pos.x,
+				item.pos.y - item.headSize*1.2);
+
+	ctx.arc(item.pos.x, 
+			item.pos.y, 
+			item.headSize, 
+			0, Math.PI*2);
+	ctx.rect(item.pos.x-item.headSize*0.5, 
+			item.pos.y+item.headSize*0.6, 
+			item.headSize, 
+			item.headSize*1.8)
+	ctx.rect(item.pos.x-item.headSize*0.2, 
+			item.pos.y+item.headSize*2.4, 
+			item.headSize*0.4, 
+			item.headSize)
+	ctx.arc(item.pos.x, 
+			item.pos.y+item.headSize*3.4, 
+			item.headSize*0.3, 
+			0, Math.PI*2);
+	if (item.kick) {
+		var that = item;
+		if (item.p_no == 0) {
+			var dir = 0;
+		} else if (item.p_no == 1){
+			var dir = item.headSize*1.4;
+		}
+		ctx.rect(item.pos.x-dir, 
+			item.pos.y+item.headSize*3.1, 
+			item.headSize*1.4, 
+			item.headSize*0.6)
+	}
+
+	ctx.fillStyle = item.colour;
+	ctx.fill();
+	ctx.closePath();
+
+	drawGoalPost(item.goalPost);
+}
+
+var drawGoalPost = function (item) {
+	ctx.beginPath();
+	ctx.rect(item.pos.x,
+			item.pos.y,
+			item.w,
+			item.h);
+	ctx.strokeStyle = '#000000';
+	ctx.lineWidth = 8;
+
+	ctx.stroke();
+	ctx.closePath();
+}
+
+
+var drawBall = function (item) {
+	ctx.beginPath();
+	ctx.arc(item.pos.x, 
+			item.pos.y, 
+			item.size, 
+			0, Math.PI*2);
+	ctx.fillStyle = '#000000';
+	ctx.fill();
+	ctx.closePath();
+}
+
+
+var drawScore = function () {
+	ctx.font = "60px Comic Sans MS";
+	ctx.fillStyle = "grey";
+	ctx.textAlign = "center";
+	ctx.fillText(text, canvas.width/2, canvas.height/4);
+	ctx.fillText(score_p1.toString()+'-'+score_p2.toString(),
+				canvas.width/2, canvas.height/3);
+}
+
+
+var update = function(){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	drawBall(ball);
+	for (var i=0; i<players.length; i++) {
+		drawPlayer(players[i]);
+	}
+}
+
+
+//setInterval(update,1000/60);
+function repeat() {
+	//console.log(p1.pos.x)
+  	update()
+  	if (scored) {
+		drawScore();
+		
+	}
+  	requestAnimationFrame(repeat);
+}
+repeat()
+
+
 var socket = io.connect();
 
 socket.on('connect', function () {
@@ -8,8 +199,96 @@ socket.on('onconnected', function(data){
 	if (data.avail) {
 		console.log('You have joined the game, with id:' +
 					data.id);
+		syncClient(data.game);
+		p_no = data.p_no;
+		players[p_no].p_no = p_no;
 	} else {
 		console.log('Lobby is full. You are an observer with id:' +
 					data.id);
 	}
+	
+	
+
+	if (data.p_no == 0) {
+		observer = false;
+		console.log('You are now Player 1.');
+		p_name = prompt('You are now Player 1 (Red). ' +
+			'Move using the WASD keys, where A is left, '+
+			'D is right, W is jump, and D is kick. ' +
+			'Enter your desired username below.', 'Rick');
+		socket.emit('p_name_update', {p_name : p_name,
+									p_no : data.p_no});
+		document.addEventListener("keydown", 
+				function(e){keyDown(e,p1);}, false);
+		document.addEventListener("keyup", 
+				function(e){keyUp(e,p1);}, false);
+	} else if (data.p_no == 1) {
+		observer = false;
+		console.log('You are now Player 2.');
+		p_name = prompt('You are now Player 2 (Blue). '+
+			'Move using the arrow keys, '+
+			'where up is jump, and down is kick. '+
+			'Enter your desired username below.', 'Morty');
+		socket.emit('p_name_update', {p_name : p_name,
+									p_no : data.p_no});
+		document.addEventListener("keydown", 
+				function(e){keyDown(e,p2);}, false);
+		document.addEventListener("keyup", 
+				function(e){keyUp(e,p2);}, false);
+	} else if (data.p_no == 2) {
+		observer = true;
+		console.log('You are now an observer.');
+		alert('You are now an observer. When someone leaves, ' +
+			'refresh page to join game.');
+	}
 });
+
+
+socket.on('p_disconnect', function(server) {
+	if (server.p_no == 0) {
+		alert('Player 1 has disconnected.');
+	} else if (server.p_no == 1) {
+		alert('Player 2 has disconnected.');
+	}
+});
+
+socket.on('sync', function(server){
+	syncClient(server);
+});
+
+
+var syncClient = function(server){
+	for (var i=0; i < players.length; i++) {
+		var p = players[i];
+		p.pos.x = server.players[i].pos.x;
+		p.pos.y = server.players[i].pos.y;
+		p.goalPost.pos.x = server.players[i].goalPost.pos.x;
+		p.goalPost.pos.y = server.players[i].goalPost.pos.y;
+		p.kick = server.players[i].kick;
+		p.ctrls = server.players[i].ctrls;
+		p.p_name = server.players[i].p_name;
+	}
+	ball.pos.x = server.ball.pos.x;
+	ball.pos.y = server.ball.pos.y;
+	text = server.text;
+	score_p1 = server.score_p1;
+	score_p2 = server.score_p2;
+	scored = server.scored;
+};
+
+var sendData = function(){
+	if (p_no == 0) {
+		socket.emit('sendData', {player:p1});
+		p1.downPress = false;
+	} else if (p_no == 1) {
+		socket.emit('sendData', {player:p2})
+		p2.downPress = false;
+	}
+};
+
+setInterval(function(){
+	if (observer) {
+	} else {
+	sendData();
+	}
+},1000/60)
--- server.js
+++ server.js
@@ -13,41 +13,612 @@ var server = app.listen(8082, function () {
 var io = require('socket.io')(server);
  
 function GameServer(){
-	this.players = [];
-	this.ball = null;
+	this.players = players;
+	this.ball = ball;
+	this.p_names = ['','']
+	this.text = text;
+	this.scored = scored;
+	this.score_p1 = score_p1;
+	this.score_p2 = score_p2;
 	this.lobby = false;
+	this.avail = true;
+	this.free_p_no = [0,0];
 }
 
 GameServer.prototype = {
 
 	checkAvail: function(){
-		if (this.players.length == 0) {
+		var no_players = this.free_p_no[0] + 
+						this.free_p_no[1];
+		if (no_players == 0) {
 			this.lobby = false;
-			return true;
-		} else if (this.players.length == 1) {
+			this.avail = true;
+		} else if (no_players == 1) {
 			this.lobby = true;
-			return true;
-		} else {
-			return false;
+			this.avail = true;
+		} else if (no_players == 2) {
+			this.avail = false;
+		}
+	},
+
+	refresh : function () {
+		this.players = players;
+		this.ball = ball;
+		this.text = text;
+		this.scored = scored;
+		this.score_p1 = score_p1;
+		this.score_p2 = score_p2;
+	},
+
+	updateNames : function (clientData) {
+		this.p_names[clientData.p_no] = clientData.p_name;
+		this.players[clientData.p_no].p_name = clientData.p_name;
+	},
+
+	syncServer: function(clientData){
+		if (clientData.player.p_no == 0){
+			players[0].upPress = clientData.player.upPress;
+			players[0].downPress = clientData.player.downPress;
+			players[0].leftPress = clientData.player.leftPress;
+			players[0].rightPress = clientData.player.rightPress;
+		} else if (clientData.player.p_no == 1)  {
+			players[1].upPress = clientData.player.upPress;
+			players[1].downPress = clientData.player.downPress;
+			players[1].leftPress = clientData.player.leftPress;
+			players[1].rightPress = clientData.player.rightPress;
 		}
 	}
 }
 
 
 var game = new GameServer();
-
+var canvas = {
+	width : 1600,
+	height : 900}
 /* Connection events */
 
 io.on('connection', function(client) {
 	client.userid = UUID();
+	game.checkAvail();
+	if (!game.lobby) {
+		client.p_no = 0;
+		game.free_p_no = [1,0];
+		resetMatch();
+	} else if (game.lobby && game.free_p_no[0]
+						&& !game.free_p_no[1]) {
+		client.p_no = 1;
+		game.free_p_no = [1,1];
+		resetScore();
+	} else if (game.lobby && !game.free_p_no[0]
+						&& game.free_p_no[1]) {
+		client.p_no = 0;
+		game.free_p_no = [1,1];
+		resetScore();
+	} else {
+		client.p_no = 2;
+	}
 
     console.log('\t User connected, id: ' + client.userid);
 
     client.emit('onconnected', {id:client.userid,
-    							avail:game.checkAvail(),
-    							lobby:game.lobby)}
+    							avail:game.avail,
+    							p_no:client.p_no,
+    							game:game})
+    client.on('p_name_update', function(clientData){
+    	game.updateNames(clientData);
+    })
+    client.on('sendData', function(clientData){
+    	game.syncServer(clientData);
+    	client.emit('sync', {players:game.players,
+    								ball:game.ball,
+    								text:game.text,
+    								score_p1:score_p1,
+    								score_p2:score_p2,
+    								scored:scored})
+    	client.broadcast.emit('sync', {players:game.players,
+    								ball:game.ball,
+    								text:game.text,
+    								score_p1:score_p1,
+    								score_p2:score_p2,
+    								scored:scored})
+    })
 
 	client.on('disconnect', function () {
+		game.free_p_no[client.p_no] = 0;
         console.log('\t User disconnected, id: ' + client.userid);
+        client.broadcast.emit('p_disconnect', {p_no : client.p_no});
     });
 });
+
+
+
+
+// Controls are up, down, left, right.
+var p1_ctrls = new Array(87,83,65,68);
+var p2_ctrls = new Array(38,40,37,39);
+var mspf = 1000/100 // Milliseconds per frame.
+var tmod = 0.3*(60/1000)*mspf; // Speed/acceleration modifier.
+
+
+Object.prototype.clone = Array.prototype.clone = function()
+{
+    return JSON.parse(JSON.stringify(this));
+}
+
+var Vector2d  = function (x,y) {
+	this.x = x;
+	this.y = y;
+};
+Vector2d.prototype.add = function(v) {
+	this.x += v.x;
+	this.y += v.y;
+};
+Vector2d.prototype.mult = function(s) {
+	this.x *= s;
+	this.y *= s;
+};
+Vector2d.prototype.set = function(x,y) {
+	this.x = x;
+	this.y = y;
+};
+Vector2d.prototype.dot = function(u) {
+	return this.x*u.x + this.y*u.y;
+};
+Vector2d.prototype.sub = function(u) {
+	return new Vector2d(this.x-u.x, this.y-u.y);
+};
+
+var CircCollider = function(x,y,r) {
+	this.x = x;
+	this.y = y;
+	this.r = r;
+};
+CircCollider.prototype.check = function (coll) {
+	if (coll instanceof CircCollider) {
+		if (Math.sqrt((this.x - coll.x)*(this.x - coll.x)
+			+ (this.y - coll.y)*(this.y - coll.y)) < 
+			(this.r + coll.r)) {
+			return true;
+		} else {
+			return false;
+		}
+	} else if (coll instanceof RectCollider) {
+		var distX = Math.abs(this.x - coll.x - coll.w/2);
+   		var distY = Math.abs(this.y - coll.y - coll.h/2);
+
+    	if (distX > (coll.w/2 + this.r)) {
+      	 	return false;
+    	}
+    	if (distY > (coll.h/2 + this.r)) {
+        	return false;
+    	}
+
+    	if (distX <= (coll.w/2)) {
+       		return true;
+    	}
+    	if (distY <= (coll.h/2)) {
+        	return true;
+    	}
+
+    	var dx = distX - coll.w/2;
+    	var dy = distY - coll.h/2;
+    	return (dx*dx + dy*dy <= (this.r*this.r));
+	}
+};
+CircCollider.prototype.update = function (x,y) {
+	this.x = x;
+	this.y = y;
+};
+
+var RectCollider = function(x,y,w,h) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+};
+RectCollider.prototype.check = function (coll) {
+	if (coll instanceof RectCollider) {
+		if (this.x <= coll.x + coll.w &&
+   			this.x + this.w >= coll.x &&
+  			this.y <= coll.y + coll.h &&
+   			this.h + this.y >= coll.y) {
+			return true;
+		}
+	} else if (coll instanceof CircCollider) {
+		var distX = Math.abs(coll.x - this.x - this.w/2);
+   		var distY = Math.abs(coll.y - this.y - this.h/2);
+
+    	if (distX > (this.w/2 + coll.r)) {
+      	 	 return false;
+    	}
+    	if (distY > (this.h/2 + coll.r)) {
+        	return false;
+    	}
+
+    	if (distX <= (this.w/2)) {
+       		return true;
+    	}
+    	if (distY <= (this.h/2)) {
+        	return true;
+    	}
+
+    	var dx = distX - this.w/2;
+    	var dy = distY - this.h/2;
+    	return (dx*dx + dy*dy <= (coll.r*coll.r));
+	}
+};
+RectCollider.prototype.update = function (x,y) {
+	this.x = x;
+	this.y = y;
+};
+
+
+var Player = function(x,y,ctrls,colour,p_no){
+	this.pos = new Vector2d(x,y); // position of head centre
+	this.vel = new Vector2d(0,0);
+	this.acc = new Vector2d(0,0);
+	this.last_pos = new Vector2d(0,0);
+	this.p_name = '';
+	this.mass = 60;
+	this.headSize = 40;
+	this.init(x,y,ctrls,colour,p_no);
+};
+
+Player.prototype.init = function(x,y,ctrls,colour,p_no) {
+	this.ctrls = ctrls;
+	this.p_no = p_no;
+	this.colour = colour;
+	this.upPress = false;
+	this.downPress = false;
+	this.leftPress = false;
+	this.rightPress = false;
+
+	this.pos.set(x,y);
+	this.vel.set(0,0);
+
+	this.headColl = new CircCollider(this.pos.x, 
+									this.pos.y, 
+									this.headSize)
+	this.bodyColl = new RectCollider(this.pos.x-
+									this.headSize*0.5, 
+									this.pos.y, 
+									this.headSize, 
+									this.headSize*2.3)
+	this.footColl = new CircCollider(this.pos.x, 
+									this.pos.y+
+									this.headSize*3.4, 
+									this.headSize*0.3)
+
+	
+	this.goalPost = new GoalPost(p_no,
+						this.headSize*4,
+						this.headSize*9);
+
+	this.frozen = false;
+	this.kick = false;
+	this.canJump = true;
+	this.jumpSpd = -20*tmod;
+};
+
+Player.prototype.update = function() {
+	if (this.pos.y < canvas.height - this.headSize*3.7){
+		this.acc.y = 1.5*tmod*tmod;
+		this.canJump = false;
+	} else {
+		this.pos.y = canvas.height - this.headSize*3.7;
+		this.acc.y = 0;
+		this.vel.y = 0;
+		this.canJump = true;
+	}
+
+	if (this.rightPress && !this.leftPress) {
+		if (Math.sign(this.vel.x) == -1){
+			this.acc.x = 3*tmod*tmod;
+		} else {
+			this.acc.x = 1.5*tmod*tmod;
+		}
+	} else if (this.leftPress && !this.rightPress) {
+		if (Math.sign(this.vel.x) == 1){
+			this.acc.x = -3*tmod*tmod;
+		} else {
+			this.acc.x = -1.5*tmod*tmod;
+		}
+	} else if (this.pos.y < 
+				canvas.height-this.headSize*3.7) {
+		this.acc.x = 0;
+	} else if (Math.abs(this.vel.x) > 1*tmod) {
+		this.acc.x = -Math.sign(this.vel.x)*2*tmod;
+	} else {
+		this.acc.x = 0;
+		this.vel.x = 0;
+	}
+
+
+	if (this.frozen){
+		this.vel.x = 0;
+		this.acc.x = 0;
+	}
+
+	if (this.canJump && this.upPress){
+		this.vel.y = this.jumpSpd;
+		this.canJump = false;
+		this.jumpTimer=0;
+	}
+
+	if (this.pos.x - this.headSize < 0) {
+		this.pos.x = 0 + this.headSize;
+		this.vel.x = -0.5*this.vel.x;
+		this.acc.x = 0;
+	} else if (this.pos.x + this.headSize > canvas.width) {
+		this.pos.x = canvas.width - this.headSize;
+		this.vel.x = -0.5*this.vel.x;
+		this.acc.x = 0;
+	}
+
+	this.last_pos.x = this.pos.x.clone();
+	this.last_pos.y = this.pos.y.clone();
+	this.pos.add(this.vel);
+	this.vel.add(this.acc);
+
+	this.headColl.update(this.pos.x, 
+						this.pos.y)
+	this.bodyColl.update(this.pos.x-
+						this.headSize*0.5, 
+						this.pos.y+
+						this.headSize*0)
+	this.footColl.update(this.pos.x, 
+						this.pos.y+
+						this.headSize*3.4)
+};
+
+
+
+var Ball = function(x,y){
+	this.pos = new Vector2d(x,y); // position of head centre
+	this.vel = new Vector2d(0,0);
+	this.acc = new Vector2d(0,1.5*tmod*tmod);
+	this.last_pos = new Vector2d(0,0);
+	this.mass = 5;
+	this.size = 30; 
+	this.init(x,y);
+};
+
+Ball.prototype.init = function(x,y) {
+	this.pos.set(x,y);
+	this.vel.set(20*(Math.random()-0.5)*tmod,0);
+	this.coll = new CircCollider(this.pos.x, 
+									this.pos.y, 
+									this.size)
+	this.elast = 0.7;
+};
+
+Ball.prototype.update = function(players) {
+	if (this.pos.y < this.size) {
+		this.pos.y = this.size;
+		this.vel.y = -this.elast*this.vel.y;
+	} else if (this.pos.y >= canvas.height - this.size) {
+		this.pos.y = canvas.height - this.size;
+		this.vel.y = -this.elast*this.vel.y;
+	}
+
+	if (Math.abs(this.vel.x) > 1*tmod &&
+		this.pos.y >= canvas.height - this.size) {
+		this.acc.x = -Math.sign(this.vel.x)*2*tmod;
+	} else {
+		this.acc.x = 0;
+	}
+
+	if (this.pos.x - this.size < 0) {
+		this.pos.x = 0 + this.size;
+		this.vel.x = -0.8*this.vel.x;
+		this.acc.x = -this.acc.x;
+	} else if (this.pos.x + this.size > canvas.width) {
+		this.pos.x = canvas.width - this.size;
+		this.vel.x = -0.8*this.vel.x;
+		this.acc.x = -this.acc.x;
+	}
+
+
+	for (var i=0; i < players.length; i++) {
+		var p = players[i];
+		var sizes = this.size + p.headSize;
+		if (this.coll.check(p.bodyColl)) {
+				var vel_init = this.vel.x.clone();
+				this.vel.x = (p.mass/(this.mass+p.mass)*
+							(p.vel.x - this.vel.x));
+				p.vel.x = -(this.mass/(this.mass+p.mass)*
+							(p.vel.x - vel_init));
+
+				this.pos.x = this.last_pos.x.clone();
+				this.pos.y = this.last_pos.y.clone();
+				p.pos.x = p.last_pos.x.clone();
+				//p.pos.y = p.last_pos.y.clone();
+		}else if (this.coll.check(p.headColl) || 
+			this.coll.check(p.footColl)) {
+			if (this.coll.check(p.headColl)) {
+				var posP = p.pos;
+				p.pos.x = p.last_pos.x.clone();
+				//p.pos.y = p.last_pos.y.clone();
+			} else {
+				var posP = new Vector2d(p.pos.x, 
+							p.pos.y+3.4*p.headSize);
+				p.pos.x = p.last_pos.x.clone();
+				p.pos.y = p.last_pos.y.clone();
+			}
+
+			var velB = this.vel;
+			var posB = this.pos;
+			var velP = p.vel;
+			var mB = this.mass;
+			var mP = p.mass;
+			
+			this.vel.x =  this.vel.x - ((2*mP/(mB+mP))*(
+				velB.sub(velP).dot(posB.sub(posP)))/
+				posB.sub(posP).dot(posB.sub(posP)))*
+				posB.sub(posP).x;
+			this.vel.y = this.vel.y - ((2*mP/(mB+mP))*(
+				velB.sub(velP).dot(posB.sub(posP)))/
+				posB.sub(posP).dot(posB.sub(posP)))*
+				posB.sub(posP).y;
+
+			p.vel.x =  p.vel.x - ((2*mB/(mB+mP))*(
+				velP.sub(velB).dot(posP.sub(posB)))/
+				posP.sub(posB).dot(posP.sub(posB)))*
+				posP.sub(posB).x;
+			p.vel.y =  p.vel.y - ((2*mB/(mB+mP))*(
+				velP.sub(velB).dot(posP.sub(posB)))/
+				posP.sub(posB).dot(posP.sub(posB)))*
+				posP.sub(posB).y;
+
+			this.pos.x = this.last_pos.x.clone();
+			this.pos.y = this.last_pos.y.clone();
+
+		}
+
+		if (p.downPress) {
+			p.kick = true;
+			if (p.p_no == 0) {
+				setTimeout(function(){
+				p1.kick = false;
+				}, 100);
+			} else {
+				setTimeout(function(){
+				p2.kick = false;
+				}, 100);
+			}
+			p.downPress = false;
+			if (Math.sqrt(((this.pos.x - p.pos.x)*
+				(this.pos.x - p.pos.x)) + 
+				((this.pos.y-p.pos.y-3.4*p.headSize)*
+				(this.pos.y-p.pos.y-3.4*p.headSize))) < 
+				(this.size + 1.4*p.headSize) &&
+				this.pos.y+this.size > p.pos.y-p.headSize*3.7) {
+				if (p.p_no == 0 && this.pos.x > p.pos.x) {
+					this.vel.y -= 30*tmod;
+					this.vel.x += 30*tmod;
+				} else if (p.p_no == 1 && this.pos.x < p.pos.x) {
+					this.vel.y -= 30*tmod;
+					this.vel.x -= 30*tmod;
+				}
+			}
+		}
+
+		if (this.coll.check(p.goalPost.topColl)) {
+			if (this.coll.check(p.goalPost.cornerColl)) {
+				this.vel.x = -this.vel.x;
+			} else {
+				this.vel.y = -this.vel.y;
+			}
+			//this.pos.x = this.last_pos.x.clone();
+			this.pos.y = this.last_pos.y.clone();
+		} else if (this.coll.check(p.goalPost.goalColl)) {
+			restartGame(p.p_no);
+		}
+	}
+	this.last_pos.x = this.pos.x.clone();
+	this.last_pos.y = this.pos.y.clone();
+	this.pos.add(this.vel);
+	this.vel.add(this.acc);
+	this.coll.update(this.pos.x, this.pos.y);
+};
+
+var GoalPost = function(p_no,w,h) {
+	this.w = w;
+	this.h = h;
+	this.p_no = p_no;
+	this.init(p_no,w,h);
+};
+GoalPost.prototype.init = function (p_no,w,h) {
+	if (this.p_no == 0) {
+		var pos_x = 0;
+		var corner_x = this.w - 10;
+		var goal_w = 0;
+	} else if (this.p_no == 1) {
+		var pos_x = canvas.width - this.w;
+		var corner_x = pos_x;
+		var goal_w = 30;
+	}
+	this.topColl = new RectCollider(
+					pos_x,
+					canvas.height - this.h,
+					this.w,
+					10);
+	this.cornerColl = new RectCollider(
+					corner_x,
+					canvas.height - this.h,
+					10,
+					10);
+	this.goalColl = new RectCollider(
+					pos_x+goal_w,
+					canvas.height - this.h + 10,
+					this.w-30, //Ball size.
+					this.h - 10);
+	this.pos = {};
+	this.pos.x = this.topColl.x;
+	this.pos.y = this.topColl.y;
+};
+
+var p1 = new Player(canvas.width*0.2,canvas.height,
+					p1_ctrls,'red',0);
+var p2 = new Player(canvas.width*0.8,canvas.height,
+					p2_ctrls,'blue',1);
+var players = [p1,p2];
+var ball = new Ball (canvas.width/2, 
+					canvas.height*0.1)
+
+
+var text = '';
+var score_p1 = 0;
+var score_p2 = 0;
+var scored = false;
+
+var resetScore = function () {
+	text = '';
+	score_p1 = 0;
+	score_p2 = 0;
+	scored = false;
+}
+
+var resetMatch = function() {
+	p1 = new Player(canvas.width*0.2,canvas.height,
+					p1_ctrls,'red',0);
+	p2 = new Player(canvas.width*0.8,canvas.height,
+					p2_ctrls,'blue',1);
+	players = [p1,p2];
+	ball = new Ball (canvas.width/2, 
+					canvas.height*0.1)
+}
+
+var restartGame = function (p_no) {
+	scored = true;
+	setTimeout(function(){scored = false;}, 2000);
+	if (p_no == 1) {
+		text = 'Player 1 Scored!';
+		score_p1 += 1;
+	} else if (p_no == 0) {
+		text = 'Player 2 Scored!';
+		score_p2 += 1;
+	}
+	p1 = new Player(canvas.width*0.2,canvas.height,
+					p1_ctrls,'red',0);
+	p2 = new Player(canvas.width*0.8,canvas.height,
+					p2_ctrls,'blue',1);
+	p1.p_name = game.p_names[0];
+	p2.p_name = game.p_names[1];
+	players = [p1,p2];
+	ball = new Ball (canvas.width/2, 
+					canvas.height*0.1)
+}
+
+var update = function(){
+	for (var i=0; i<players.length; i++) {
+		players[i].update();
+	}
+	ball.update(players);
+}
+
+// Run physics at 100 fps, update at 60 fps.
+setInterval(function(){
+	update();
+	game.refresh();
+	},mspf);
